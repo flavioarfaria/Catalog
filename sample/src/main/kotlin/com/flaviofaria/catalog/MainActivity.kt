c24d45a170707e04234a0bc767d2aec8c5f6c3b1
Flávio Faria <flavioarfaria@gabrielas-mbp.lan>
2022-05-13 00:28:22 -0400

    Validate context receiver behavior

--- sample/src/main/kotlin/com/flaviofaria/catalog/MainActivity.kt
+++ sample/src/main/kotlin/com/flaviofaria/catalog/MainActivity.kt
@@ -1,13 +1,26 @@
 package com.flaviofaria.catalog
 
+import android.content.Context
 import android.os.Bundle
+import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
+import com.flaviofaria.catalog.sample.R
 
 class MainActivity : AppCompatActivity() {
 
   override fun onCreate(savedInstanceState: Bundle?) {
     super.onCreate(savedInstanceState)
+    setContentView(TextView(this).apply {
+      text = Strings.appName()
+    })
   }
 
+  object Strings {
+
+    context(Context)
+    fun appName(): String {
+      return getString(R.string.app_name)
+    }
+  }
 
 }
