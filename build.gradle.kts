d2721c1684d6b43ebd3d062ec874557c9497e7da
Flávio Faria <flavioarfaria@gabrielas-mbp.lan>
2022-05-13 00:33:05 -0400

    Introduce compiler module

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,4 +2,5 @@ plugins {
     id("com.android.application") version "7.2.0" apply false
     id("com.android.library") version "7.2.0" apply false
     kotlin("android") version "1.6.20" apply false
+    id("org.jetbrains.kotlin.jvm") version "1.6.21" apply false
 }
--- settings.gradle.kts
+++ settings.gradle.kts
@@ -14,3 +14,4 @@ dependencyResolutionManagement {
 }
 rootProject.name = "Catalog"
 include(":sample", ":catalog")
+include(":compiler")
