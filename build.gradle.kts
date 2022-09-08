0aa82ac98e2514dccba568d7910e1e3feff0d5de
Flávio Faria <flavioarfaria@flavios-mbp.lan>
2022-09-08 00:23:28 -0400

    Update gradle

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,6 +1,6 @@
 plugins {
-    id("com.android.application") version "7.2.0" apply false
-    id("com.android.library") version "7.2.0" apply false
+    id("com.android.application") version "7.2.2" apply false
+    id("com.android.library") version "7.2.2" apply false
     kotlin("android") version "1.6.21" apply false
     id("org.jetbrains.kotlin.jvm") version "1.6.21" apply false
 }
