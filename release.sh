964e6668ea0269f6e74fd35d1e5ec2665b64bbaa
Flávio Faria <flavioarfaria@gmail.com>
2022-11-20 23:28:52 -0500

    Update release script to include publishing properties

--- release.sh
+++ release.sh
@@ -2,4 +2,5 @@
 sed -i '' -e "s/id 'com.flaviofaria.catalog' version '$1'/id 'com.flaviofaria.catalog' version '$2'/g" README.md
 sed -i '' -e "s/RUNTIME_VERSION = \"$1\"/RUNTIME_VERSION = \"$2\"/g" catalog-gradle-plugin/src/main/java/com/flaviofaria/catalog/gradle/CatalogPlugin.kt
 sed -i '' -e "s/catalog = \"$1\"/catalog = \"$2\"/g" gradle/libs.versions.toml
+sed -i '' -e "s/VERSION_NAME=$1/VERSION_NAME=$2/g" gradle.properties
 
